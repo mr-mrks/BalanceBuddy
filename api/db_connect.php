1d0d084be9841070d6abd113d0ba2eae68aaf768
Markus <81853046+mr-mrks@users.noreply.github.com>
2025-03-03 14:35:37 +0000

    Update db_connect.php

--- api/db_connect.php
+++ api/db_connect.php
@@ -3,7 +3,7 @@ function get_db_connection() {
     static $db;
     if ($db === null) {
         try {
-            $db = new SQLite3('../balancebuddy.db');
+            $db = new SQLite3('BalanceBuddy/balancebuddy.db');
         } catch (Exception $e) {
             error_log("Database connection error: " . $e->getMessage());
             return null;
